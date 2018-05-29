ac0cc51b5a7cf308ccc7ca8b28b70c9f69b91fc3
Taha Zacholi <tzach011@gold.ac.uk>
2018-05-29 03:05:29 +0100

    healthbar

--- VR-Dest/Assets/Script/Break.cs
+++ VR-Dest/Assets/Script/Break.cs
@@ -3,71 +3,72 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Break : MonoBehaviour {
+public class Break : MonoBehaviour
+{
     Rigidbody rb; //reference to the rigidbody, if we call RB we instantly know RB = rigidbody
     public int ToolSpeed;
-    public float StartHealth = 20f;
-    public float health;
+    float StartHealth = 20f;
+    float health;
 
-    public Image HealthBar;
-    public GameObject HealthBarShow;
-    
-    
-	// Use this for initialization
-	void Start () {
+    //public GameObject HealthBarShow;
+
+
+    // Use this for initialization
+    void Start()
+    {
         rb = gameObject.GetComponent<Rigidbody>(); // when we call RB we already know its the rigidbody but here we set the RB to getting the component for easy acces.
         health = StartHealth;
-
     }
 
     // Update is called once per frame
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
 
         ToolSpeed = HammerScript.speed;
         RayCastThis(); //FixedUpdate for Physics. We call the Raycast function
-        //GameObject.Find("Sledge").GetComponent<HammerScript>().speed = ToolSpeed;
+                       //GameObject.Find("Sledge").GetComponent<HammerScript>().speed = ToolSpeed;
 
         //  GameObject.Find("Hammer").GetComponent<HammerScript>().speed = ToolSpeed;
-        //Debug.Log(Glass);
-       // Debug.Log(ToolSpeed);
+        // Debug.Log(health);
+        // Debug.Log(ToolSpeed);
 
-	}
+    }
 
     void RayCastThis()
     {
-       
+
         if (gameObject.tag == "Breakable") // If the tag is Breakable then the raycasting is applied to that object. If the tag is anything else. The raycast isnt applied
         {
             RaycastHit hit; //Create the raycast here
             if (Physics.Raycast(gameObject.transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity)) //We have a Physics Raycast here that takes in parameters.
-                //First one is the gameobject the that the script is attached to middlepoint(Position). The second one is the direction we point the raycast. Third is the ray we send and last is the length of the raycast
+                                                                                                                                     //First one is the gameobject the that the script is attached to middlepoint(Position). The second one is the direction we point the raycast. Third is the ray we send and last is the length of the raycast
             {
                 if (hit.transform.tag == "Ground") //Simple control statement. If the raycast hits the object with "ground" as the tag the kinematic on the objects gets turned off (Which makes it fall)
                 {
                     rb.isKinematic = false;
-                   // Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow); //<---- Debug statement for getting displaying the raycast in Yellow lines
-                    
+                    // Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow); //<---- Debug statement for getting displaying the raycast in Yellow lines
+
                 }
             }
         }
 
     }
- 
+
     void OnCollisionEnter(Collision Coll)
     {
 
-        if(Coll.gameObject.tag == "Hammer" && ToolSpeed > 3)
+        if (Coll.gameObject.tag == "Hammer" && ToolSpeed > 3)
         {
-            health -=  10f;
+            health -= 10f;
             rb.isKinematic = false;
-            HealthBar.fillAmount = health / StartHealth;
-            
+
+
 
 
             Debug.Log(health);
-            if(health < 10f)
+            if (health < 10f)
             {
-                
+
                 StartCoroutine(DestroyWithDelay());
                 rb.AddForce(Coll.impulse * 1, ForceMode.Impulse);
             }
@@ -79,6 +80,9 @@ public class Break : MonoBehaviour {
         Destroy(gameObject);
     }
 
+    public float GetDividedHealth()
+    {
+        return health / StartHealth;
+    }
 
-   
 }
---
+++ VR-Dest/Assets/Script/RayCastCam.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.UI;
+
+public class RayCastCam : MonoBehaviour
+{
+
+    public float RayDistance = 500;
+    public GameObject SetActivePls;
+    public Image HealthBar;
+
+    public float GetHealth;
+    public float GetStarterHealth;
+    // Use this for initialization
+    void Start()
+    {
+        //rend = SetActivePls.GetComponent<Renderer>();
+
+    }
+    void Update()
+    {
+        // GetHealth = Break.health;
+        // GetStarterHealth = Break.StartHealth;
+        Debug.Log(GetHealth);
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Ray ray= Camera.main.ScreenPointToRay(InputTracking.GetLocalPosition(XRNode.CenterEye));
+        // HealthBar.fillAmount = GetHealth / GetStarterHealth;
+        RaycastHit hit;
+
+        if (Physics.Raycast(gameObject.transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            if (hit.transform.tag == ("Base"))
+            {
+
+                SetActivePls.GetComponent<Canvas>().enabled = true;
+                Break breakScript = hit.transform.GetComponent<Break>();
+                HealthBar.fillAmount = breakScript.GetDividedHealth(); ;
+
+            }
+            else
+            {
+                SetActivePls.GetComponent<Canvas>().enabled = false;
+            }
+
+            //else
+            //{
+            //    SetActivePls.GetComponent<Canvas>().enabled = false;
+            //    Debug.Log("hit");
+            //}
+
+        }
+    }
+
+}
